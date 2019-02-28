8cdd24088a54c50c3f6a7d420091371fe51231d7
jackbenn <jack@bennetto.com>
2019-02-27 22:34:59 -0800

    implement variables and operators

--- mouse.py
+++ mouse.py
@@ -1,9 +1,22 @@
 import sys
+import operator
 
 class MouseParser:
+
+    ops = {'+': operator.add,
+           '-': operator.sub,
+           '*': operator.mul,
+           '/': operator.floordiv}
     def __init__(self):
         self.stack = []
+        self.variables = [None] * 26
     
+    def push(self, x):
+        self.stack.append(x)
+    
+    def pop(self):
+        return self.stack.pop()
+
     def parse(self, text):
         
         i = 0
@@ -15,8 +28,19 @@ class MouseParser:
                     i += 1
                     num *= 10
                     num += int(text[i])
-                self.stack.append(num)
-
+                self.push(num)
+            elif char in self.ops:
+                a = self.pop()
+                b = self.pop()
+                self.push(self.ops[char](b, a))
+            elif 'a' <= char <= 'z':
+                self.push(ord(char) - ord('a'))
+            elif char == '.':
+                self.push(self.variables[self.pop()])
+            elif char == '=':
+                a = self.pop()
+                b = self.pop()
+                self.variables[b] = a
             i += 1
 
 if __name__ == '__main__':
