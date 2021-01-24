6f05623a4110bc72be08528f16ec2dc0a2a86eb2
Jack Bennetto <jack@bennetto.com>
2021-01-23 18:33:59 -0800

    start progress on supporting macros (incomplete)

--- src/mouse.py
+++ src/mouse.py
@@ -3,9 +3,10 @@ import operator
 import time
 
 class StackFrame:
-    def __init__(self):
-        self.variables = [None] * 26
-        self.location
+    def __init__(self, type, pos, base):
+        self.type = type
+        self.base = base
+        self.pos = pos
 
 class MouseParser:
     ops = {'+': operator.add,
@@ -15,10 +16,13 @@ class MouseParser:
            '\\': operator.mod,
            '=': operator.eq,
            '<': operator.lt,
-           '>': operator.gt}
+           '>': operator.gt
+           '=': operator.eq}
     def __init__(self):
         self.stack = []
         self.loop_stack = []
+    
+        self.variable_base = 0
         self.variables = [None] * 26
     
     def push(self, x):
@@ -26,13 +30,26 @@ class MouseParser:
 
     def pop(self):
         return self.stack.pop()
+    
+    def get_variable(self, i):
+        return self.variables[i + self.variable_base * 26]
 
+    def get_variable(self, i, value):
+        self.variables[i + self.variable_base * 26] = value
+    
     def find_next(self, text, i, char):
         """Return next instance of char in the text starting at i"""
         return text
+    
+    def push_frame(self, ):
+        
+
+    def pop_frame(self):
+
+        return 
 
     def parse(self, text):
-        
+        """parse and execute a mouse program"""
         i = 0
         while i < len(text):
             char = text[i]
@@ -58,6 +75,8 @@ class MouseParser:
                         print()
                     else:
                         print(text[i], end='')
+            elif char == '?':
+                self.push(int(input()))
             elif 'a' <= char <= 'z':
                 self.push(ord(char) - ord('a'))
             elif char == '.':
@@ -93,6 +112,5 @@ if __name__ == '__main__':
     parser = MouseParser()
     text = sys.stdin.read()
     parser.parse(text)
-
     print(parser.stack)
     print(parser.variables)
