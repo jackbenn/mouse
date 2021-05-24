48c25cb54ff1af1505da8cde4cca0231589e65be
Jack Bennetto <jack@bennetto.com>
2021-05-24 14:03:08 -0700

    working towards macros (in progress?)

--- src/mouse.py
+++ src/mouse.py
@@ -4,9 +4,9 @@ import time
 
 class StackFrame:
     def __init__(self, type, pos, base):
-        self.type = type
-        self.base = base
+        self.type = type  # 'macro', 'param', 'loop'
         self.pos = pos
+        self.variable_base = variable_base
 
 class MouseParser:
     ops = {'+': operator.add,
@@ -24,6 +24,8 @@ class MouseParser:
     
         self.variable_base = 0
         self.variables = [None] * 26
+        self.frames = []
+
     
     def push(self, x):
         self.stack.append(x)
@@ -34,19 +36,34 @@ class MouseParser:
     def get_variable(self, i):
         return self.variables[i + self.variable_base * 26]
 
-    def get_variable(self, i, value):
+    def set_variable(self, i, value):
         self.variables[i + self.variable_base * 26] = value
     
     def find_next(self, text, i, char):
         """Return next instance of char in the text starting at i"""
         return text
     
-    def push_frame(self, ):
-        
+    def push_frame(self, type, pos, base):
+
+        frame = StackFrame(type, pos, self.variable_base)
+
+        # find new location....somehow
+
+        self.frames.append(frame)
+        if type=='param':
+            self.variable_base -= 1
+        elif type=='macro':
+            self.variable_base += 1
+        elif type=='loop':
+            pass
+        else:
+            raise Exception("Invalid frame type")
 
     def pop_frame(self):
 
-        return 
+        frame = self.frames.pop()
+        self.variable_base = frame.base
+        return frame.pos
 
     def parse(self, text):
         """parse and execute a mouse program"""
@@ -80,12 +97,13 @@ class MouseParser:
             elif 'a' <= char <= 'z':
                 self.push(ord(char) - ord('a'))
             elif char == '.':
-                self.push(self.variables[self.pop()])
+                self.push(self.get_variable(self.pop())
             elif char == ':':
                 a = self.pop()
                 b = self.pop()
-                self.variables[a] = b
+                self.set_variable(a, b)
             elif char == '(':
+                frame = StackFrame('loop', i, )
                 self.loop_stack.append(i)
             elif char == ')':
                 i = self.loop_stack.pop() - 1
